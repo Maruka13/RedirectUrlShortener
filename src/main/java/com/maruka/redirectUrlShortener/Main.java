b8af7f3f0961b0467fad17627e3e691647df350e
Maruka <emanuelle_carollina@hotmail.com>
2024-11-23 14:42:30 -0300

    Requisiçao de get no bucket

--- src/main/java/com/maruka/redirectUrlShortener/Main.java
+++ src/main/java/com/maruka/redirectUrlShortener/Main.java
@@ -3,7 +3,9 @@ package com.maruka.redirectUrlShortener;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
 
+import java.io.InputStream;
 import java.util.Map;
 
 
@@ -18,6 +20,19 @@ public class Main implements RequestHandler<Map<String, Object>, Map<String, Str
 
             if (shortUrlCode == null || shortUrlCode.isEmpty()) {
                 throw new IllegalArgumentException("Invalid input: 'shortUrlCode' is required.");
+            }
+
+            GetObjectRequest getObjectRequest = GetObjectRequest.builder()
+                    .bucket("maruka-urlshortener-storage")
+                    .key(shortUrlCode + "json")
+                    .build();
+
+            InputStream s3objectStream;
+
+            try{
+                s3objectStream = s3Client.getObject(getObjectRequest);
+            } catch (Exception e) {
+                throw new RuntimeException("Error fetching data from s3: " + e.getMessage(), e);
             }
 
             return null;
