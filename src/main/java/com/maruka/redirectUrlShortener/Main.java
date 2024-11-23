76d84f39c1e314f938adc01edce1df0d1c9ea842
Maruka <emanuelle_carollina@hotmail.com>
2024-11-23 14:31:08 -0300

    ClientS3 p consultar

--- src/main/java/com/maruka/redirectUrlShortener/Main.java
+++ src/main/java/com/maruka/redirectUrlShortener/Main.java
@@ -2,11 +2,14 @@ package com.maruka.redirectUrlShortener;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import software.amazon.awssdk.services.s3.S3Client;
 
 import java.util.Map;
 
 
 public class Main implements RequestHandler<Map<String, Object>, Map<String, String>> {
+
+        private final S3Client s3Client = S3Client.builder().build();
 
         @Override
         public Map<String, String> handleRequest(Map<String, Object> input, Context context) {
