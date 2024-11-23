1a80e754152680d8d1951630b89728c01a7d4a80
Maruka <emanuelle_carollina@hotmail.com>
2024-11-23 15:38:06 -0300

    tempo inválido + correção

--- src/main/java/com/maruka/redirectUrlShortener/Main.java
+++ src/main/java/com/maruka/redirectUrlShortener/Main.java
@@ -13,52 +13,57 @@ import java.util.Map;
 
 public class Main implements RequestHandler<Map<String, Object>, Map<String, Object>> {
 
-        private final S3Client s3Client = S3Client.builder().build();
+    private final S3Client s3Client = S3Client.builder().build();
 
-        private final ObjectMapper objectMapper = new ObjectMapper();
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
-        @Override
-        public Map<String, Object> handleRequest(Map<String, Object> input, Context context) {
-            String pathParameters = (String) input.get("rawPath");
-            String shortUrlCode = pathParameters.replace("/", "");
+    @Override
+    public Map<String, Object> handleRequest(Map<String, Object> input, Context context) {
+        String pathParameters = (String) input.get("rawPath");
+        String shortUrlCode = pathParameters.replace("/", "");
 
-            if (shortUrlCode == null || shortUrlCode.isEmpty()) {
-                throw new IllegalArgumentException("Invalid input: 'shortUrlCode' is required.");
-            }
-
-            GetObjectRequest getObjectRequest = GetObjectRequest.builder()
-                    .bucket("maruka-urlshortener-storage")
-                    .key(shortUrlCode + "json")
-                    .build();
-
-            InputStream s3objectStream;
-
-            try{
-                s3objectStream = s3Client.getObject(getObjectRequest);
-            } catch (Exception e) {
-                throw new RuntimeException("Error fetching data from s3: " + e.getMessage(), e);
-            }
-
-            UrlData urlData;
-
-            try {
-                urlData = objectMapper.readValue(s3objectStream, UrlData.class);
-            } catch (Exception e) {
-                throw new RuntimeException("Error deserializing URL data: " + e.getMessage(), e);
-            }
-
-            long currentTimeInSeconds = System.currentTimeMillis() / 1000;
-            if (currentTimeInSeconds < urlData.getExpirationTime()) {
-                Map<String, Object> response = new HashMap<>();
-                response.put("statusCode", 302);
-                Map<String, String> headers = new HashMap<>();
-                headers.put("Location", urlData.getOriginalUrl());
-                headers.put("headers", String.valueOf(headers));
-
-                return response;
-            }
-
-            return null;
+        if (shortUrlCode == null || shortUrlCode.isEmpty()) {
+            throw new IllegalArgumentException("Invalid input: 'shortUrlCode' is required.");
         }
 
+        GetObjectRequest getObjectRequest = GetObjectRequest.builder()
+                .bucket("maruka-urlshortener-storage")
+                .key(shortUrlCode + "json")
+                .build();
+
+        InputStream s3objectStream;
+
+        try {
+            s3objectStream = s3Client.getObject(getObjectRequest);
+        } catch (Exception e) {
+            throw new RuntimeException("Error fetching data from s3: " + e.getMessage(), e);
+        }
+
+        UrlData urlData;
+
+        try {
+            urlData = objectMapper.readValue(s3objectStream, UrlData.class);
+        } catch (Exception e) {
+            throw new RuntimeException("Error deserializing URL data: " + e.getMessage(), e);
+        }
+
+        long currentTimeInSeconds = System.currentTimeMillis() / 1000;
+
+        Map<String, Object> response = new HashMap<>();
+
+        if (urlData.getExpirationTime() < currentTimeInSeconds) {
+            response.put("statusCode", 410);
+            response.put("body", "This URL was expired");
+
+        } else {
+            response.put("statusCode", 302);
+            Map<String, String> headers = new HashMap<>();
+            headers.put("Location", urlData.getOriginalUrl());
+            response.put("headers", headers);
+
+            return response;
+        }
+
+        return null;
+    }
 }
